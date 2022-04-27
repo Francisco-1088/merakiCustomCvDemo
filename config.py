ff48a39abf15a5e4cab363222787be9a77e30a17
Francisco-1088 <32076369+Francisco-1088@users.noreply.github.com>
2022-04-27 14:26:21 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+
+# Parameters for cvSnapper.py
+api_key = ''
+camera_serial = ''
+desired_snaps = 100
+dataset_name = ''
+train_test_split = 0.80
+
+# Parameters for mvCustomCvDashboard.py
+max_classes = 8
+mqtt_server = '127.0.0.1'
+det_threshold = 0.75
